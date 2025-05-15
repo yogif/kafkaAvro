85759416ff9e41e5aa02f2a6f4abbb9d05b6f7d1
yogif <61491178+yogif@users.noreply.github.com>
2025-05-15 17:15:50 +0700

    Add files via upload

---
+++ src/test/java/com/activity/KafkaSchemaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSchemaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
